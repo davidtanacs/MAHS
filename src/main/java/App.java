e132c67b6ab43fad85c62dbd6335be7652c01959
davidtanacs <tanacsdavid90@gmail.com>
2017-10-01 15:15:24 +0200

    Add app.java and rendered main menu.

---
+++ src/main/java/App.java
@@ -0,0 +1,26 @@
+import spark.Request;
+import spark.Response;
+import spark.Spark.*;
+import org.thymeleaf.context.WebContext;
+import spark.template.thymeleaf.ThymeleafTemplateEngine;
+
+import static spark.Spark.*;
+import static spark.debug.DebugScreen.enableDebugScreen;
+
+public class App {
+
+    public static void main(String[] args){
+
+            // default server settings
+            exception(Exception.class, (e, req, res) -> e.printStackTrace());
+            staticFileLocation("/public");
+            port(1234);
+
+            get("/", (Request req, Response res) -> {
+                return new ThymeleafTemplateEngine().render(DataController.renderMainMenu());
+            });
+
+            enableDebugScreen();
+        }
+
+    }
