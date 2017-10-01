4a858100467aa34b952da146234593e768708af9
davidtanacs <tanacsdavid90@gmail.com>
2017-10-01 18:10:49 +0200

    Generate free appointments depens on morning or afternoon.

--- src/main/java/MassageTherapist.java
+++ src/main/java/MassageTherapist.java
@@ -1,3 +1,4 @@
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -5,15 +6,51 @@ public class MassageTherapist {
 
     massageTherapist name;
     String gender;
-    List<String> treatments;
-
+    List<Integer> treatments;
+    List<LocalTime> freeAppointments = new ArrayList<>();
+    LocalTime shiftStart;
+    LocalTime shiftEnd;
+    
+    
     MassageTherapist(massageTherapist name){
         this.name = name;
         this.gender = name.getGender();
         this.treatments = new ArrayList<>();
+        this.freeAppointments = generateAppointments();
+
+    }
+    
+    private boolean isAfternoon(){
+        if (LocalTime.now().isAfter(LocalTime.of(13,00))){
+            return true;
+        }
+        return false;
     }
 
-    public void addTreatments(String treatmentId){
+    public LocalTime getShiftStart() {
+        return shiftStart;
+    }
+
+    public LocalTime getShiftEnd() {
+        return shiftEnd;
+    }
+
+    private List generateAppointments(){
+        if (isAfternoon()){
+            shiftStart = LocalTime.of(15,30);
+            shiftEnd = LocalTime.of(20,35);
+        } else {
+            shiftStart = LocalTime.of(9,00);
+            shiftEnd = LocalTime.of(11,35);
+        }
+        while (shiftStart.isBefore(shiftEnd)){
+            freeAppointments.add(shiftStart);
+            shiftStart = shiftStart.plusMinutes(5);
+        }
+        return freeAppointments;
+    }
+
+    public void addTreatments(Integer treatmentId){
         treatments.add(treatmentId);
     }
 
