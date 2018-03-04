24fbf76f1bd7465d188d1f3e45a196b43f04af65
davidtanacs <tanacsdavid90@gmail.com>
2018-03-04 09:03:06 +0100

    Changed localtime connections to work like a class.

--- src/main/java/com/hobbyProject/mahs/service/Service.java
+++ src/main/java/com/hobbyProject/mahs/service/Service.java
@@ -4,16 +4,13 @@ import com.hobbyProject.mahs.model.*;
 import org.springframework.ui.Model;
 
 import javax.servlet.http.HttpSession;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @org.springframework.stereotype.Service
 public class Service {
 
     public String renderBookingPage(Model model, HttpSession session){
-        Map<Integer, Integer> massages = new HashMap<>();
+        List<Massage> massages = new ArrayList<>();
 
         List masseurs = new ArrayList();
         MassageTherapist Dani = new MassageTherapist(MassageTherapist.massageTherapist.Dani, new Shift());
@@ -26,11 +23,11 @@ public class Service {
         model.addAttribute("masseurs", masseurs);
 
 
-        massages.put(Massage.fifteen.length, Massage.fifteen.getPrice());
-        massages.put(Massage.thirty.length, Massage.thirty.getPrice());
-        massages.put(Massage.fourtyfive.length, Massage.fourtyfive.getPrice());
-        massages.put(Massage.sixty.length, Massage.sixty.getPrice());
-        massages.put(Massage.ninety.length, Massage.ninety.getPrice());
+        massages.add(new Massage(15));
+        massages.add(new Massage(30));
+        massages.add(new Massage(45));
+        massages.add(new Massage(60));
+        massages.add(new Massage(90));
 
         model.addAttribute("massages", massages);
 
@@ -40,7 +37,7 @@ public class Service {
         return "/booking";
     }
 
-    public void bookAMassage(Model model, Guest guest, Massage massage, int treatmentStartHour, int treatmentStartMinute, MassageTherapist massageTherapist){
+    /*public void bookAMassage(Model model, Guest guest, Massage massage, int treatmentStartHour, int treatmentStartMinute, MassageTherapist massageTherapist){
         Treatment treatment = new Treatment(massage, treatmentStartHour, treatmentStartMinute,
                 massageTherapist, guest.getId());
         massageTherapist.addTreatments(treatment.getId());
@@ -51,32 +48,10 @@ public class Service {
         System.out.println("length: " + treatment.getMassage().length  + " break after: " + treatment.getBreakAfter());
         System.out.println("guestid: " + treatment.getGuestId() + " masseur: " + treatment.getMassageTherapist());
         System.out.println("price: " + treatment.getMassage().getPrice());
-    }
+    }*/
 
 
 
-    public Massage getMassageEnumByLength(int length){
-        Massage massage = null;
 
-        switch (length){
-            case 15:
-                massage = Massage.fifteen;
-                break;
-            case 30:
-                massage = Massage.thirty;
-                break;
-            case 45:
-                massage = Massage.fourtyfive;
-                break;
-            case 60:
-                massage = Massage.sixty;
-                break;
-            case 90:
-                massage = Massage.ninety;
-                break;
-        }
-
-        return massage;
-    }
 
 }
