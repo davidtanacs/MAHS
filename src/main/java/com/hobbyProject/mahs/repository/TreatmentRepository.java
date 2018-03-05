05b026c14986ef37f574355bb579b235c3827c51
davidtanacs <tanacsdavid90@gmail.com>
2018-03-05 09:49:47 +0100

    Add repository annotation to treatmentrepo.

--- src/main/java/com/hobbyProject/mahs/repository/TreatmentRepository.java
+++ src/main/java/com/hobbyProject/mahs/repository/TreatmentRepository.java
@@ -2,7 +2,9 @@ package com.hobbyProject.mahs.repository;
 
 import com.hobbyProject.mahs.model.Treatment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TreatmentRepository extends JpaRepository<Treatment, Long>{
     
 }
